203ff839f360a8883c3d2f89d530c882b76e0ac6
codeshreyas1 <codeshreyas1@gmail.com>
2026-04-01 17:27:20 +0530

    first commit

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -3,9 +3,13 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { OrdersService } from './orders.service';
 import { OrdersController } from './orders.controller';
 import { Order, OrderSchema } from '../schemas/order.schema';
+import { WhatsappModule } from '../whatsapp/whatsapp.module';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: Order.name, schema: OrderSchema }])],
+  imports: [
+    MongooseModule.forFeature([{ name: Order.name, schema: OrderSchema }]),
+    WhatsappModule,
+  ],
   controllers: [OrdersController],
   providers: [OrdersService]
 })
